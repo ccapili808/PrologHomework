d18c69aeb4a6e7712c4e6306943015f8fc46da9d
ccapili808 <115299284+ccapili808@users.noreply.github.com>
2023-11-23 17:43:26 -0700

    Update SlidingTiles.pl

--- SlidingTiles.pl
+++ SlidingTiles.pl
@@ -29,78 +29,89 @@ a way to print each state that has been visited
 
 deciding which moves are valid
 */
-% V1 % 
-goal_state(State) :-
-    append(_, ['_', B | _], State), B \= 'W'.
-
-move(State, NewState, Cost) :-
-    nth0(EmptyIndex, State, '_'),
-    adjacent_move(State, EmptyIndex, NewState, Cost);
-    hop_move(State, EmptyIndex, NewState, Cost).
-
-adjacent_move(State, EmptyIndex, NewState, 1) :-
-    AdjacentIndex is EmptyIndex - 1,
-    swap_tiles(State, EmptyIndex, AdjacentIndex, NewState).
-
-adjacent_move(State, EmptyIndex, NewState, 1) :-
-    AdjacentIndex is EmptyIndex + 1,
-    swap_tiles(State, EmptyIndex, AdjacentIndex, NewState).
-
-hop_move(State, EmptyIndex, NewState, 1) :-
-    HopIndex is EmptyIndex - 2,
-    swap_tiles(State, EmptyIndex, HopIndex, NewState).
-
-hop_move(State, EmptyIndex, NewState, 2) :-
-    HopIndex is EmptyIndex + 2,
-    swap_tiles(State, EmptyIndex, HopIndex, NewState).
-
-swap_tiles(State, I, J, NewState) :-
-    nth0(I, State, ItemI),
-    nth0(J, State, ItemJ),
-    replace(State, I, ItemJ, TempState),
-    replace(TempState, J, ItemI, NewState).
-
-replace([_|T], 0, X, [X|T]).
-replace([H|T], I, X, [H|R]) :-
-    I > 0,
-    NI is I-1,
-    replace(T, NI, X, R).
 
 
-heuristic(State, Value) :-
-    findall(Count, (nth1(IndexW, State, 'W'), count_b_left(State, IndexW, Count)), Counts),
-    sum_list(Counts, Value).
+sum_Bs_before_Ws(List, Total) :- sum_Bs_before_Ws(List, List, 0, Total).
+sum_Bs_before_Ws([], _, Total, Total).
+sum_Bs_before_Ws(['W'|Tail], FullList, CurrentSum, Total) :-
+        count_Bs_before_W(FullList, Tail, 0, BsCount),
+        NewSum is CurrentSum + BsCount,
+        sum_Bs_before_Ws(Tail, FullList, NewSum, Total).
 
-count_b_left(State, IndexW, Count) :-
-    sublist(State, 0, IndexW, SubState),
-    include(=(B), SubState, Bs),
-    length(Bs, Count).
+sum_Bs_before_Ws([_|Tail], FullList, CurrentSum, Total) :-
+        sum_Bs_before_Ws(Tail, FullList, CurrentSum, Total).
 
-solve(State, Solution) :-
-    depth_first([], State, Solution).
+count_Bs_before_W(FullList, Tail, CurrentCount, Count) :-
+        append(Head, Tail, FullList),
+        findall(X, (member(X, Head), X = 'B'), Bs),
+        length(Bs, BsLength),
+        Count is CurrentCount + BsLength.
 
-depth_first(Path, State, [State|Path]) :-
-    goal_state(State).
+% Time to generate the children of a state, using our valid moves.
 
-depth_first(Path, State, Sol) :-
-    move(State, NewState, _),
-    \+ member(NewState, Path),
-    depth_first([State|Path], NewState, Sol).
+generate_children(State, Children) :-
+        findall(Child, (valid_move(State, Child), Child \= State), Children).
+
+valid_move(State, Child) :-
+        swap_adjacent(State, Child);
+        swap_hop_one(State, Child);
+        swap_hop_two(State, Child).
+
+% Swap with adjacent tile (both directions)
+swap_adjacent(State, Child) :-
+        (append(Prefix, ['_', Tile | Suffix], State); append(Prefix, [Tile, '_' | Suffix], State)),
+        member(Tile, ['B', 'W']),
+        (append(Prefix, [Tile, '_' | Suffix], Child); append(Prefix, ['_', Tile | Suffix], Child)).
+
+% Swap by hopping over one tile (both directions)
+swap_hop_one(State, Child) :-
+        (append(Prefix, ['_', X, Tile | Suffix], State); append(Prefix, [Tile, X, '_' | Suffix], State)),
+        member(Tile, ['B', 'W']),
+        member(X, ['B', 'W']),
+        (append(Prefix, [Tile, X, '_' | Suffix], Child); append(Prefix, ['_', X, Tile | Suffix], Child)).
+
+% Swap by hopping over two tiles (both directions)
+swap_hop_two(State, Child) :-
+        (append(Prefix, ['_', X, Y, Tile | Suffix], State); append(Prefix, [Tile, X, Y, '_' | Suffix], State)),
+        member(Tile, ['B', 'W']),
+        member(X, ['B', 'W']),
+        member(Y, ['B', 'W']),
+        (append(Prefix, [Tile, X, Y, '_' | Suffix], Child); append(Prefix, ['_', X, Y, Tile | Suffix], Child)).
 
 
+% Test that these work, BBB_WWW should in theory have six children.
+% Test passed: Print children with this command:
+% generate_children(['B', 'B', 'B', '_', 'W', 'W', 'W'], Children), print_children(Children).
 
-print_solution([]).
-print_solution([H|T]) :-
-    writeln(H),
-    print_solution(T).
+% Next step, Get the heuristic value of each child, choose the smallest and print it. This will be our new state.
+% Repeat this process until we have a heuristic value of 0
+% For tie breaking, just choose any value, won't matter.
+
+find_best_child([Child], Child).
+find_best_child([Child1, Child2 | Rest], BestChild) :-
+        sum_Bs_before_Ws(Child1, Heuristic1),
+        sum_Bs_before_Ws(Child2, Heuristic2),
+        (Heuristic1 =< Heuristic2 -> BetterChild = Child1; BetterChild = Child2),
+        find_best_child([BetterChild | Rest], BestChild).
 
 
+% Recursively call the find best child method, printing the steps we take.
+greedy_search(State) :-
+        sum_Bs_before_Ws(State, Heuristic),
+        (Heuristic = 0 -> print('Goal State Reached: '), print_list(State);
+        generate_children(State, Children),
+        find_best_child(Children, BestChild),
+        print('Chosen Child: '), print_list(BestChild), nl,
+        greedy_search(BestChild)).
 
-main :-
-    InitialState = ['B', 'B', 'B', '_', 'W', 'W', 'W'],
-    solve(InitialState, Solution),
-    reverse(Solution, OrderedSolution),
-    print_solution(OrderedSolution).
+%General Print statements
+print_list([]) :- nl.
+print_list([H|T]) :-
+            write(H), write(' '),
+            print_list(T).
+
+print_children(Children) :-
+        print_list(Children).
 
 
 
